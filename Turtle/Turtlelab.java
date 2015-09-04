4ab32f50a713d60731ab0143eb31aaa8b145ca21
hubgithub <lhengi@foxmail.com>
2015-09-03 21:27:51 -0500

    almost done
    almost done, need let the turtle shows infront of the ink.

--- Turtle/Turtlelab.java
+++ Turtle/Turtlelab.java
@@ -7,14 +7,11 @@ public class Turtlelab
     {
         World turtleWorld = new World();
         Turtle turtle1 = new Turtle(0,0,turtleWorld);
-        turtle1.setPenColor(Color.RED);
+        turtle1.setPenColor(Color.PINK);
         turtle1.setBodyColor(Color.cyan);
         turtle1.penDown();
         turtle1.setPenWidth(5);
 
-        
-        
-        
         Turtle turtle2 = new Turtle(630,520,turtleWorld);
         turtle2.setPenColor(Color.YELLOW);
         turtle2.setBodyColor(Color.red);
@@ -28,25 +25,104 @@ public class Turtlelab
         turtle3.penDown();
         turtle3.setPenWidth(5);
         
+        Turtle turtle4 = new Turtle(0,210,turtleWorld);
+        turtle4.setPenColor(Color.black);
+        turtle4.setBodyColor(Color.black);
+        turtle4.penDown();
+        turtle4.setPenWidth(5);
+        
+        Turtle turtle5 = new Turtle(320,250,turtleWorld);
+        turtle5.setPenColor(Color.YELLOW);
+        turtle5.setBodyColor(Color.RED);
+        turtle5.penDown();
+        turtle5.setPenWidth(3);
+        
         int x = 0;
 
 
         while (x<= 13)
         {   
- 
-                
-            turtle1.turn(150);
+            turtle1.turn(147);
             turtle1.forward(630);
             turtle1.turn(-300);
             turtle1.forward(630);
             x += 1;
             
-            turtle2.turn(-150);
+            turtle2.turn(-153);
             turtle2.forward(630);
             turtle2.turn(300);
             turtle2.forward(630);
-        
-        
+            
+            turtle3.turn(142);
+            turtle3.forward(630);
+            turtle3.turn(-300);
+            turtle3.forward(630);
+            
+            turtle4.turn(150);
+            turtle4.forward(630);
+            turtle4.turn(-300);
+            turtle4.forward(630);
+
+            Thread.sleep(500);
+            
     }
+    
+    int num = 0;
+    int movx = 2;
+    while (num <= 330)
+    {
+        turtle5.forward(movx);
+        turtle5.turnLeft();
+        if (num >= 50)
+        {
+            turtle5.setPenColor(Color.CYAN);
+        }
+        
+        if (num >= 100)
+        {
+            turtle5.setPenColor(Color.YELLOW);
+        }
+        
+        if (num >= 150)
+        {
+            turtle5.setPenColor(Color.MAGENTA);
+        }
+        if (num >= 200)
+        {
+            turtle5.setPenColor(Color.ORANGE);
+        }
+        if (num >= 250)
+        {
+            turtle5.setPenColor(Color.BLUE);
+        }
+        movx += 2;
+        num += 1;
+        Thread.sleep(20);
+    }
+    
+    turtle1.setBodyColor(Color.BLACK);
+    turtle1.isVisible();
+    turtle1.moveTo(300,300);
+    turtle1.setPenColor(Color.BLACK);
+    turtle1.setPenWidth(100);
+    turtle1.forward(20);
+    
+    turtle2.setBodyColor(Color.BLACK);
+    turtle3.setBodyColor(Color.BLACK);
+    turtle4.setBodyColor(Color.BLACK);
+    turtle5.setBodyColor(Color.BLACK);
+//     turtle6.setBodyColor(Color.BLACK);
+//     turtle7.setBodyColor(Color.BLACK);
+//     turtle8.setBodyColor(Color.BLACK);
+//     
+//     if ( (x & 1) == 0 )
+    
+    
+    
+    
+    
+    
+    
+    
 }
 }
